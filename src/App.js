14564510e907b92b0935df43f8a65788daa70951
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-05 17:24:17 +0100

    edited files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback  } from 'react';
 import Bird from './components/Bird';
 import GameMenu from './components/GameMenu';
 import GameLoop from './components/GameLoop';
@@ -7,30 +7,38 @@ import GameLevel from './components/GameLevel';
 import Obstacle from './components/Obstacle';
 import PowerUp from './components/PowerUp';
 import Game from './components/Game';
+import { BrowserRouter as Route, useNavigate, Router, Routes } from 'react-router-dom';
 
 const App = () => {
   const [gameState, setGameState] = useState('menu');
+  const navigate = useNavigate();
 
   const startGame = () => {
     setGameState('playing');
+    navigate('/game');
   };
 
-  if (gameState === 'menu') {
-    return <GameMenu startGame={startGame} />;
-  } else if (gameState === 'playing') {
-    return (
-      <div>
-        <Bird />
-        <Obstacle />
-        <PowerUp />
-        <Leaderboard />
-        <GameLevel />
-        <GameLoop />
-        <Game gameState={gameState} />
-      </div>
-    );
-  } else {
-    return null;
-  }
-}  
+  return (
+    <div>
+      <Router path="/">
+        <Routes>
+        {gameState === 'menu' && <Route path="/game" component={<GameMenu startGame={startGame} />} />
+}
+        {gameState === 'playing' && (
+          <div>
+            <Route path="/game" component={<Bird />} />
+            <Route path="/game" component={<Obstacle/>} />
+            <Route path="/game" component={<PowerUp/>} />
+            <Route path="/game" component={<Leaderboard/>} />
+            <Route path="/game" component={<GameLevel/>} />
+            <Route path="/game" component={<GameLoop/>} />
+            <Route path="/game" component = {<Game gameState={gameState} />} />
+          </div>
+        )}
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
 export default App;
