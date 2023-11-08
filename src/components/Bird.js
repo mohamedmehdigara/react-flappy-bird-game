9417b4efa10124b494ad9e7038784f208e3b710a
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-08 16:24:35 +0100

    edited files

--- src/components/Bird.js
+++ src/components/Bird.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const Bird = () => {
+  const [isFlapping, setIsFlapping] = useState(false);
+  
+  const handleFlap = () => setIsFlapping(!isFlapping);
+
+  
+
   return (
-    <div
-      style={{
-        position: "absolute",
-        top: 100,
-        left: 100,
-        width: 20,
-        height: 20,
-        backgroundColor: "black",
-      }}
-    />
+    <BirdContainer
+      className={`bird ${isFlapping ? 'flapping' : ''}`}
+      onMouseDown={handleFlap}
+    >
+      {isFlapping ? <AnimatedBird /> : <StaticBird />}
+    </BirdContainer>
   );
 };
+
+const BirdContainer = styled.div`
+  position: absolute;
+  top: 100px; /* Adjust the initial position as needed */
+  transition: transform 0.3s; /* Smooth transition for bird movement */
+`;
+
+const BirdBaseStyles = styled.div`
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  position: absolute;
+`;
+const flapAnimation = keyframes`
+  0% {
+    transform: translateY(-10px) rotate(0deg);
+  }
+  50% {
+    transform: translateY(10px) rotate(45deg);
+  }
+  100% {
+    transform: translateY(-10px) rotate(0deg);
+  }
+`;
+
+
+const AnimatedBird = styled(BirdBaseStyles)`
+  transform: translateY(-10px);
+  animation: ${flapAnimation} 1s infinite alternate;
+`;
+
+const StaticBird = styled(BirdBaseStyles)`
+  transform: translateY(0);
+`;
+
+
 
 export default Bird;
