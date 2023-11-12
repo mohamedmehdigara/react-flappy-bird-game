1b95dab727116aaafbc504a0ccc71179d2e6915d
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-12 11:42:29 +0100

    edited files

--- src/components/Obstacle.js
+++ src/components/Obstacle.js
@@ -1,44 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledObstacle = styled.div`
+const ObstacleContainer = styled.div`
   width: 50px;
   height: 100px;
   position: absolute;
-  background-color: ${(props) => props.color || 'green'};
+  background-color: green;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
 `;
 
 const Obstacle = ({ type, position, color }) => {
+  // Check if position is defined before accessing its properties
+  if (!position || typeof position !== 'object' || isNaN(position.y)) {
+    return null;
+  }
+
   // Render the appropriate obstacle based on the type.
   switch (type) {
     case 'pipe':
-    case 'platform':
       return (
-        <StyledObstacle
+        <ObstacleContainer
           key={position.y}
-          title={`${type} obstacle`}
+          title="Pipe obstacle"
           role="obstacle"
           style={{
             top: position.y,
             left: position.x,
+            backgroundColor: color,
           }}
-          color={color}
+        />
+      );
+    case 'platform':
+      return (
+        <ObstacleContainer
+          key={position.y}
+          title="Platform obstacle"
+          role="obstacle"
+          style={{
+            top: position.y,
+            left: position.x,
+            backgroundColor: color,
+          }}
         />
       );
     default:
       return null;
   }
-};
-
-Obstacle.propTypes = {
-  type: PropTypes.oneOf(['pipe', 'platform']).isRequired,
-  position: PropTypes.shape({
-    x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired,
-  }).isRequired,
-  color: PropTypes.string, // Add this if 'color' is an optional prop
 };
 
 export default Obstacle;
--- src/components/PowerUp.js
+++ src/components/PowerUp.js
@@ -1,47 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledPowerUp = styled.div`
+const PowerUpContainer = styled.div`
   width: 50px;
   height: 50px;
   position: absolute;
-  background-color: ${(props) => props.backgroundColor || 'blue'};
+  background-color: blue;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
 `;
 
 const PowerUp = ({ type, position, effect }) => {
-  // Define the background color based on the effect (add more if needed)
-  const backgroundColor = effect === 'speedBoost' ? 'yellow' : 'blue';
+  // Check if position is defined before accessing its properties
+  if (!position || typeof position !== 'object' || isNaN(position.y)) {
+    return null;
+  }
 
   // Render the appropriate power-up based on the type.
   switch (type) {
     case 'speedBoost':
-    case 'invincibility':
       return (
-        <StyledPowerUp
+        <PowerUpContainer
           key={position.y}
-          title={`${effect} power-up`}
+          title="Speed boost power-up"
           role="powerup"
           style={{
             top: position.y,
             left: position.x,
           }}
-          backgroundColor={backgroundColor}
+        />
+      );
+    case 'invincibility':
+      return (
+        <PowerUpContainer
+          key={position.y}
+          title="Invincibility power-up"
+          role="powerup"
+          style={{
+            top: position.y,
+            left: position.x,
+          }}
         />
       );
     default:
       return null;
   }
-};
-
-PowerUp.propTypes = {
-  type: PropTypes.oneOf(['speedBoost', 'invincibility']).isRequired,
-  position: PropTypes.shape({
-    x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired,
-  }).isRequired,
-  effect: PropTypes.string.isRequired,
 };
 
 export default PowerUp;
