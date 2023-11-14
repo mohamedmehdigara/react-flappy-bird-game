0886e6317930abc75236b066d5aff5300d5690e4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-14 16:20:07 +0100

    edited files

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,23 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+// Styled components for styling
 const BackgroundContainer = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.backgroundColor || 'skyblue'};
-  z-index: ${(props) => props.zIndex || -1}; /* Place it behind other elements */
+  background-color: ${(props) => props.backgroundColor};
+  z-index: ${(props) => props.zIndex}; /* Place it behind other elements */
 `;
 
-const Background = ({ backgroundColor = 'skyblue', zIndex = -1 }) => {
+// Background functional component
+const Background = ({ backgroundColor, zIndex }) => {
   return <BackgroundContainer backgroundColor={backgroundColor} zIndex={zIndex} />;
 };
 
+// PropTypes for type checking
 Background.propTypes = {
   backgroundColor: PropTypes.string,
   zIndex: PropTypes.number,
+};
+
+// Default props for better readability
+Background.defaultProps = {
+  backgroundColor: 'skyblue',
+  zIndex: -1,
 };
 
 export default Background;
